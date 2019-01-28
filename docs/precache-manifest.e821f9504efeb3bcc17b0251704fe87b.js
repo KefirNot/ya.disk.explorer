a93eb2013e921a3a436c63b34a7fd6666a6090a1
Alexandr Nikiforov <kefirnot@gmail.com>
2019-01-28 23:58:58 +0700

    build for demo

---
+++ docs/precache-manifest.e821f9504efeb3bcc17b0251704fe87b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9b05e87a26ef35ae9075",
+    "url": "/static/js/main.9b05e87a.chunk.js"
+  },
+  {
+    "revision": "5710cda707b1078d52cd",
+    "url": "/static/js/1.5710cda7.chunk.js"
+  },
+  {
+    "revision": "9b05e87a26ef35ae9075",
+    "url": "/static/css/main.07365d21.chunk.css"
+  },
+  {
+    "revision": "ef1bf897f158df20dce11b88b1063d31",
+    "url": "/index.html"
+  }
+];
