2b824ebb690199cdfceee09d916ff5d42b7e56fc
Alexandr Nikiforov <kefirnot@gmail.com>
2019-01-31 15:22:13 +0700

    redux-logger installed and configured

--- src/reduxConfig.js
+++ src/reduxConfig.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+import { createLogger } from 'redux-logger'
 import reducer from './store/reducer';
 import saga from './store/saga';
 
@@ -9,7 +10,7 @@ const sagaMiddleware = createSagaMiddleware(saga);
 
 const store = createStore(
     reducer,
-    applyMiddleware(sagaMiddleware),
+    applyMiddleware(sagaMiddleware, createLogger({ collapsed: true })),
 );
 
 sagaMiddleware.run(saga);
