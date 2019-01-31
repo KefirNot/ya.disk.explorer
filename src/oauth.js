b3dc41523f9273c6562e9444cee0da683a23414b
Alexandr Nikiforov <kefirnot@gmail.com>
2019-01-31 16:15:22 +0700

    auth route storing

--- src/oauth.js
+++ src/oauth.js
@@ -6,20 +6,22 @@ import { mapDispatchToProps } from './store/actions';
 
 class OAuth extends React.Component {
     redirectToOauth = () => {
+        const { location: { pathname } } = this.props;
         const OAUTH_ARGS = {
             response_type: 'token',
             client_id: process.env.REACT_APP_CLIENT_ID,
+            state: pathname,
         };
         const OAUTH_URL = `${process.env.REACT_APP_OAUTH_URL}?${qs.stringify(OAUTH_ARGS)}`;
 
         window.location.href = OAUTH_URL;
     }
 
-    fetchInitialData = (token) => {
+    fetchInitialData = (token, path) => {
         const { fetchInitialData, history } = this.props;
 
         fetchInitialData(token);
-        history.replace('/');
+        history.replace(path);
     }
 
     componentDidMount() {
@@ -28,14 +30,14 @@ class OAuth extends React.Component {
         if (!token && !hashObj.access_token) {
             this.redirectToOauth();
         } else if (hashObj.access_token) {
-            this.fetchInitialData(hashObj.access_token);
+            this.fetchInitialData(hashObj.access_token, hashObj.state);
         }
     }
 
     render() {
         const { children, token } = this.props;
 
-        if(!token) return 'loading...';
+        if (!token) return 'loading...';
 
         return children;
     }
