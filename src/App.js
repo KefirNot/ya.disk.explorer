f88d517c3a4379f3d44b6d2037444f782ed6b226
Alexandr Nikiforov <kefirnot@gmail.com>
2019-02-01 12:50:59 +0700

    removed material-ui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import ReduxConfig from './reduxConfig';
-import ThemeConfig from './themeConfig';
 import OAuth from './oauth';
 import DiskInfo from './diskInfo';
 import Dir from './dir';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
     render() {
         return (
             <ReduxConfig>
-                <ThemeConfig>
                     <BrowserRouter>
                         <OAuth>
                             <DiskInfo />
                             <Route render={(props) => <Dir key={props.location.key} {...props} />} />
                         </OAuth>
                     </BrowserRouter>
-                </ThemeConfig>
             </ReduxConfig>
         );
     }
--- src/components/loading.js
+++ src/components/loading.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import CircularProgress from '@material-ui/core/CircularProgress';
+import { Alert } from 'react-bootstrap';
 import './loading.scss';
 
 export default () => (
     <div className='loading'>
-        <CircularProgress
-            size={80}
-            thickness={7.2}
-        />
+        <Alert variant='success'>loading</Alert>
     </div>
 );
