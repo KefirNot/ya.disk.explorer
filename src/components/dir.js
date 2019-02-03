cbddfa137a6aebd8afe58e9d23d46cea16102301
Alexandr Nikiforov <kefirnot@gmail.com>
2019-02-04 00:27:24 +0700

    icons + dir style improved

--- src/components/dir.js
+++ src/components/dir.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Path from './path';
 import { connect } from 'react-redux';
-import { Card, ListGroup, ListGroupItem } from 'react-bootstrap';
+import { Card, ListGroup, ListGroupItem, Row, Col } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFolder, faFile } from '@fortawesome/free-solid-svg-icons'
 import { mapDispatchToProps } from '../store/actions';
 import Loading from './loading';
 import './dir.scss';
+
+const FolderIcon = () => <FontAwesomeIcon icon={faFolder} size='2x' color='#fdefb3' />
+const FileIcon = () => <FontAwesomeIcon icon={faFile} size='2x' color='#d8d8d8' />
 
 class Dir extends React.Component {
     componentDidMount() {
@@ -17,6 +22,17 @@ class Dir extends React.Component {
         const { history } = this.props;
 
         history.push(dir.replace('disk:', ''));
+    };
+
+    sizeToString = (size) => {
+        let n = 0;
+        const cutSize = (x) => {
+            if (x < 1024 || n === 3) return x;
+            n++;
+            return cutSize(x / 1024);
+        }
+        const unitNames = ['Б', 'КБ', 'МБ', 'ГБ',];
+        return `${Math.floor(cutSize(size))}${unitNames[n]}`;
     }
 
     render() {
@@ -30,15 +46,29 @@ class Dir extends React.Component {
                     {
                         loading
                             ? <Loading />
-                            : data.map((x, i) => (
-                                <ListGroupItem
-                                    key={i}
-                                    className='dir__obj--folder'
-                                    onClick={x.type === 'dir' ? this.makeClickFolderHandler(x.path) : null}
-                                >
-                                    {x.name}
-                                </ListGroupItem>
-                            ))
+                            : data.map((x, i) => {
+                                const isDir = x.type === 'dir';
+                                const Icon = isDir ? FolderIcon : FileIcon;
+                                return (
+                                    <ListGroupItem key={i} className={`dir__obj--${isDir ? 'folder' : 'file'}`} onClick={isDir ? this.makeClickFolderHandler(x.path) : null}>
+                                        <Row>
+                                            <Col xs={2} md={2} lg={1} className='dir__icon'>
+                                                <Icon />
+                                            </Col>
+                                            <Col xs={10} md={10} lg={11}>
+                                            <Row>
+                                                <Col md={9} xs={12}>
+                                                    {x.name}
+                                                </Col>
+                                                <Col md={3} xs={12} className='dir__size'>
+                                                    {isDir ? null : this.sizeToString(x.size)}
+                                                </Col>
+                                            </Row>
+                                            </Col>
+                                        </Row>
+                                    </ListGroupItem>
+                                );
+                            })
                     }
                 </ListGroup>
             </Card>
